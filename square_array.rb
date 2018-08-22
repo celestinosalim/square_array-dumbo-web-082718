25f78a06d7d8f1c51c55a11038306f0ba00cd95d
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 04:31:53 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   squared_array = []
-  array.each { |element| squared_array << **2 }
+  array.each { |element| squared_array << element ** 2 }
 end
 
 # def square_array(array)
